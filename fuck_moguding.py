2f56c2b3d4c94ea91db030ae2a063bd455c678b4
Agape <a741398387@gmail.com>
2021-01-11 01:48:12 +0800

    自动获取PlanID 外置config文件 qmsg酱推送支持 签到位置可从json修改 热更新位置（needs more debugs）

--- fuck_moguding.py
+++ fuck_moguding.py
@@ -4,28 +4,66 @@ import json
 import time
 import requests
 
-qmsg_api = "CHANGE_TO_YOUR_OWN_QMSG_API"
+
+def load_login():
+    with open('loginfo.json', 'r') as f:
+        data = json.load(f)
+        return data
 
 
-def qq_msg(text, api):
-    print("sending.....")
-    print(requests.get("https://qmsg.zendee.cn/send/" + api + "?msg=" + text))
+def load_location():
+    with open('location.json', 'r',encoding='utf-8') as f:
+        data = json.load(f)
+        return data
+
+phone = load_login()['phone']
+password = load_login()['password']
+msg_type = load_login()['msg_type']
+msg_token = load_login()['msg_token']
+
+def qq_msg(text, msg_token):
+    print("sending message.....")
+    requests.get("https://qmsg.zendee.cn/send/" + msg_token + "?msg=" + text)
 
 
-def sign_in(token, tpye, contexts):
+def get_planId(token,contexts):
+    request_plan_id = ur.Request(
+        url='https://api.moguding.net:9000/practice/plan/v1/getPlanByStu',
+        data=json.dumps({"paramsType": "student"}).encode("utf-8"),
+        headers={
+            'Authorization': token,
+            'Content-Type': 'application/json; charset=UTF-8',
+            'roleKey': 'student'
+        }
+    )
+
+    try:
+        plan_id = json.loads(ur.urlopen(request_plan_id, context=contexts).read().decode())['data'][0]['planId']
+        # print(plan_id)
+        if json.loads(ur.urlopen(request_plan_id, context=contexts).read().decode())['code'] == 200:
+            return plan_id
+        else:
+            with open('~/fail.txt', 'a+') as f:
+                f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + '获取token失败\n')
+    except Exception as e:
+        pass
+
+
+def sign_in(token, status, contexts, plan_id):
     data_dict = {
         "device": "WP",
-        "planId": "CHANGE_TO_YOUR_PLAN_ID",
+        "planId": plan_id,
         "country": "中国",
-        "address": "河南省 · 郑州市 · 河南省教育厅",
-        "longitude": "113.759339",
-        "city": "郑州市",
-        "latitude": "34.77359",
-        "province": "河南省",
+        "address": load_location()['address'],
+        "longitude": load_location()['longitude'],
+        "city": load_location()['city'],
+        "latitude": load_location()['latitude'],
+        "province": load_location()['province'],
         "attendanceType": "",
-        "type": "START",
+        "type": status,
         "state": "NORMAL"
     }
+    print(data_dict)
     url = 'https://api.moguding.net:9000/attendence/clock/v1/save'
     headers = {
         'Authorization': token,
@@ -46,8 +84,9 @@ def sign_in(token, tpye, contexts):
 
 def login(type, context):
     login_data = {
-        "phone": "CHANGE_TO_YOUR_PHONE",
-        "password": "CHANGE_TO_YOUR_PASSWORD",
+        "phone": phone,
+        "password": password,
+        "uuid": "",
         "loginType": "WP"
     }
     request_login = ur.Request(
@@ -58,29 +97,31 @@ def login(type, context):
         }
     )
     try:
-        token = json.loads(ur.urlopen(request_login, context=context).read().decode())['data']['token']
+        tokens = json.loads(ur.urlopen(request_login, context=context).read().decode())
+        token = tokens['data']['token']
         if token:
-            sign_in(token, type, context)
+            planid = get_planId(token, contexts)
+            sign_in(token, type, context,planid)
     except Exception as e:
         datad = '<urlopen error Remote end closed connection without response>'
         if datad == str(e):
-            # print('网络连接超时')
-            qq_msg("签到失败，原因：网络连接超时.当前时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            print('网络连接超时')
+            qq_msg("签到失败，原因：网络连接超时.当前时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),msg_token)
         else:
-            # print('账号或密码输入错误')
-            qq_msg("签到失败，原因：账号或密码输入错误.当前时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            print('账号或密码输入错误')
+            qq_msg("签到失败，原因：账号或密码输入错误.当前时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),msg_token)
         pass
 
 
 if __name__ == '__main__':
+
     while (True):
         print("------start---------")
-        context = ssl._create_unverified_context()
-        qq_msg("正在给傻逼蘑菇钉打个上班卡，当前时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), qmsg_api)
-        login("START", context)
+        contexts = ssl._create_unverified_context()
+        qq_msg("正在给傻逼蘑菇钉打个上班卡，当前时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), msg_token)
+        login("START", contexts)
         time.sleep(15)
-        qq_msg("正在给傻逼蘑菇钉打个下班卡，当前时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), qmsg_api)
-
-        login("END", context)
+        qq_msg("正在给傻逼蘑菇钉打个下班卡，当前时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), msg_token)
+        login("END", contexts)
         print("------stop----------")
         time.sleep(36000)
